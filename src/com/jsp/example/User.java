f53b578103610705e1011f1c991679753fa942dd
shyamalmahata720 <shyamalmahata720@gmail.com>
2023-12-14 14:39:28 +0530

    created some new properties in the user

--- src/com/jsp/example/User.java
+++ src/com/jsp/example/User.java
@@ -4,6 +4,8 @@ public class User {
 	private int userId;
 	private String userName;
 	private String email;
+	private String password;
+	private long phonoNo;
 	public String getEmail() {
 		return email;
 	}
@@ -21,6 +23,12 @@ public class User {
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	public long getPhonoNo() {
+		return phonoNo;
+	}
+	public void setPhonoNo(long phonoNo) {
+		this.phonoNo = phonoNo;
 	}
 	
 }
